c144072082ed609d1bf1e25d37829dfccde8484a
gb234243 <>
2018-04-02 00:59:09 +0200

    Simplify get_light_state for simulator usage

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -125,6 +125,8 @@ class TLDetector(object):
     def get_light_state(self, light):
         """Determines the current color of the traffic light
 
+           (adapted from Udacity SDC-ND Programming a Real Self-Driving Car 
+            Project Walkthrough (Term 3))
         Args:
             light (TrafficLight): light to classify
 
@@ -133,14 +135,16 @@ class TLDetector(object):
                  (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        # TODO: Replace with call to classifier
+        return light.state
+        #if(not self.has_image):
+        #    self.prev_light_loc = None
+        #    return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        #return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines 
