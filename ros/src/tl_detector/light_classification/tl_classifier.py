02acfdc43f31f2838c9050e6bccff793fe4f7320
gb234243 <>
2018-05-01 14:08:57 +0200

    Cleanup

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -12,25 +12,8 @@ class TLClassifier(object):
     def __init__(self):
         # Store the path
         model_path = rospkg.RosPack().get_path('tl_detector')
-        # model_path += '/light_classification/model/tl-classifier-frozen.pb'
         model_path += '/light_classification/model/frozen_faster_rcnn_sim.pb'
         # model_path += '/light_classification/model/frozen_ssd_inceptionv2_sim.pb'
-
-        # starttime = time.time()
-        # # Load the graph model
-        # self.graph = self.load_graph(model_path)
-
-        # # Create TF sessions
-        # self.sess = tf.Session(graph=self.graph)
-
-        # # Define input and output Tensors for graph
-        # self.image_tensor = self.graph.get_tensor_by_name('input_images:0')
-        # self.output = self.graph.get_tensor_by_name('output:0')
-        # #for p in self.graph.get_operations():
-        # #    print(p.name)
-        # endtime = time.time()
-        # print("Classifier intitializiation took {:.5f} seconds".format(endtime - starttime))
-
 
         starttime = time.time()
         self.graph = self.load_graph(model_path)
@@ -58,8 +41,9 @@ class TLClassifier(object):
             self.tensor_dict = tensor_dict
         endtime = time.time()
         print("Classifier intitializiation took {:.5f} seconds".format(endtime - starttime))
-        
-        # self.get_classification(np.zeros((600, 800, 3), dtype=np.uint8)) # Attempt to lose the 2 second start time...
+
+        # Attempt to lose the 2 second start time...
+        # self.get_classification(np.zeros((600, 800, 3), dtype=np.uint8)) 
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -69,17 +53,6 @@ class TLClassifier(object):
             int: ID of traffic light color 
                  (specified in styx_msgs/TrafficLight)
         """
-        # # Resize and normalize the image
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # image = np.array(cv2.resize(image, (300, 300)))
-        # image = image / 128.0 - 1.0
-
-        # starttime = time.time()
-        # idx = self.sess.run(tf.argmax(self.output, 1), 
-        #                     feed_dict={self.image_tensor: [image]})[0]
-        # endtime = time.time()
-        # print("Inference took {:.4f} seconds".format(endtime - starttime))
-
         # Convert the Image's Color Space
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -105,7 +78,6 @@ class TLClassifier(object):
         endtime = time.time()
         print("Inferencing took {:.5f} secs".format(endtime - starttime))
 
-        #['NoLight', 'Red', 'Yellow', 'Green']
         if idx == 2:
             print("Red")
             return TrafficLight.RED
